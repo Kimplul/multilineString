5377887f1fe5757907d59f783ef09e6c9e03c357
Kimplul <IHaveNoGoodEmail@CppRules.com>
2019-07-25 16:24:53 +0300

    Changed the order of arguments in multiLineString.h so that the buffer is the first argument.

--- multiLineString.h
+++ multiLineString.h
@@ -4,9 +4,9 @@
 #include <string.h>
 #include <stdarg.h>
 
-void concat(size_t num, char* buf, ...){
+void concat(char* buf, size_t num ...){
     va_list arg;
-    va_start(arg, buf);
+    va_start(arg, num);
     for(size_t i = 1; i < num; i++){
         strcat(buf, va_arg(arg, const char*));
         strcat(buf, "\n");
